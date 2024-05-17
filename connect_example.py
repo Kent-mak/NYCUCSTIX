1c2e376f3744824a0acc736ff7be34f17b888837
Kent <leekent173@gmail.com>
2024-05-17 23:36:00 +0800

    update connect_example.py

--- connect_example.py
+++ connect_example.py
@@ -1,15 +1,26 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from DBClient import DBClient
 
-uri = "mongodb+srv://developer:cs15@cstix1.qizcotr.mongodb.net/?retryWrites=true&w=majority&appName=CSTIX1"
-
+DB_URI = "mongodb+srv://developer:cs15@cstix1.qizcotr.mongodb.net/?retryWrites=true&w=majority&appName=CSTIX1"
+DB_NAME = 'Dev'
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+db_client = DBClient(DB_URI, DB_NAME)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+
+
+def sample_query():
+    events = db_client.find(collection_name='Events') #events is a list of objects
+    print(events)
+
+
+if __name__ == '__main__':
+    # Send a ping to confirm a successful connection
+    try:
+        db_client.ping()
+        print(f"Pinged. Successfully connected to {DB_NAME}")
+    except Exception as e:
+        print(e)
+
+    sample_query()
