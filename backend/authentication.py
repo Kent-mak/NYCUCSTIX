dbd7b89cc9f7edc8e50db8c3dbd307d6c784350b
annguo1106 <annguo1106@gmail.com>
2024-06-10 23:13:12 +0800

    backend + login (unfinished)

---
+++ backend/authentication.py
@@ -0,0 +1,20 @@
+from jose import jwt
+from passlib.context import CryptContext
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+JWT_SECRTE_KEY = config["JWT_SECRTE_KEY"]
+JWT_ALGORITHM = config["JWT_ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    encode_jwt = jwt.encode(to_encode, JWT_SECRTE_KEY, algorithm=JWT_ALGORITHM)
+    return encode_jwt
--- backend/database.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://developer:cs15@cstix1.qizcotr.mongodb.net/?retryWrites=true&w=majority&appName=CSTIX1')
-
-db = client['Dev']
-
-collection_name = db['Events']
