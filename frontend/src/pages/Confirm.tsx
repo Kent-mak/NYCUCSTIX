3f0173cf1af3520a69d9bec8fd1f623bbcb8d990
benson0402 <benson930402@gmail.com>
2024-06-19 03:12:14 +0800

    merged with kent

--- frontend/src/pages/Confirm.tsx
+++ frontend/src/pages/Confirm.tsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import NavBar from "./NavBar";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useTicket } from './TicketContext';
+// import { useTicket } from './TicketContext';
 import { useAuth } from "../provider/AuthProvider"; 
 import UserNavBar from "./UserNavBar";
 
 const Confirm: React.FC = () => {
-  const { ticketCount } = useTicket();
+  // const { ticketCount } = useTicket();
   const navigate = useNavigate();
   const handleNextClick = () => {navigate('/confirmed');};
   const location = useLocation();
